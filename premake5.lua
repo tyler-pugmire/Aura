e5c28b275742d998c1aee274a993455529e0a383
Tyler Pugmire <tyler.pugmire@yahoo.com>
2023-10-04 20:20:40 -0700

    fix linker errors on linux

--- premake5.lua
+++ premake5.lua
@@ -21,40 +21,13 @@ flags
 
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
+group "vendor"
+include "Aura/vendor/glfw"
 group ""
-project "sandbox"
--- location "sandbox"
-kind "ConsoleApp"
-language "C++"
-cppdialect "C++20"
-staticruntime "off"
-
-targetdir("bin/" .. outputdir .. "/%{prj.name}")
-objdir("bin-int/" .. outputdir .. "/%{prj.name}")
-
-links
-{
-    "Aura"
-}
-
-files
-{
-    "%{prj.name}/src/**.h",
-    "%{prj.name}/src/**.c",
-    "%{prj.name}/src/**.hpp",
-    "%{prj.name}/src/**.cpp"
-}
-
-includedirs
-{
-    "%{prj.name}/src",
-    "Aura/src",
-}
-
 
 group "Engine"
 project "Aura"
--- location "Aura"
+location "Aura"
 kind "StaticLib"
 cppdialect "C++20"
 staticruntime "off"
@@ -79,6 +52,33 @@ links
     "~/project/Aura/bin/" .. outputdir .. "/GLFW/libGLFW.a",
 }
 
-group "vendor"
-include "Aura/vendor/glfw"
 group ""
+project "sandbox"
+location "sandbox"
+kind "ConsoleApp"
+language "C++"
+cppdialect "C++20"
+staticruntime "off"
+
+targetdir("bin/" .. outputdir .. "/%{prj.name}")
+objdir("bin-int/" .. outputdir .. "/%{prj.name}")
+
+links
+{
+    "Aura",
+    "GLFW"
+}
+
+files
+{
+    "%{prj.name}/src/**.h",
+    "%{prj.name}/src/**.c",
+    "%{prj.name}/src/**.hpp",
+    "%{prj.name}/src/**.cpp"
+}
+
+includedirs
+{
+    "%{prj.name}/src",
+    "Aura/src",
+}
